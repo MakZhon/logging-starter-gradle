a9e705d080998462aa461d97a1c73fb70f4823b1
MakZhon <abovezer0lol@gmail.com>
2024-06-11 00:12:15 +0200

    init

---
+++ src/main/java/org/zhenmax/LoggingConfigurationGradle.java
@@ -0,0 +1,7 @@
+package org.zhenmax;
+
+public class LoggingConfigurationGradle {
+    public static void testLog(String str) {
+        System.out.printf("Logging From Gradle artifactory %s", str);
+    }
+}
